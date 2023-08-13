1112c22e2e8a9693b53bb6ff6ed965f870f7e07d
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2023-08-13 16:16:44 -0500

    fixed program name

--- emu/build.zig
+++ emu/build.zig
@@ -17,7 +17,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "byte-width-instructions",
+        .name = "tundra-emu",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = .{ .path = "src/main.zig" },
